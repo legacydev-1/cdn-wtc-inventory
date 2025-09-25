6cdd66f4bb614cc327ad758b6bdaf048d2baa287
legacydev-1 <protugaofgaming@gmail.com>
2025-09-26 00:30:41 +0200

    Update: 26-09-2025 0:30:40,87

--- update.bat
+++ update.bat
@@ -5,7 +5,6 @@ set BRANCH_NAME=main
 for /f "tokens=1-4 delims=/ " %%a in ("%date% %time%") do (
     set CURRENT_DATE=%%a-%%b-%%c
     set CURRENT_TIME=%%d
-    set CURRENT_TIME=!CURRENT_TIME:~0,8!
 )
 set COMMIT_MESSAGE=Update: %CURRENT_DATE% %CURRENT_TIME%
 
@@ -14,27 +13,6 @@ if not exist ".git" (
     git init
     git remote add origin %REPO_URL%
     git branch -M %BRANCH_NAME%
-)
-
-echo Vérification des modifications non validées...
-git status --porcelain
-if errorlevel 1 (
-    echo Erreur lors de la vérification de l'état du dépôt.
-    pause
-    exit /b
-)
-for /f %%i in ('git status --porcelain') do (
-    echo Modifications non validées détectées. Ajout et validation automatiques...
-    git add .
-    git commit -m "Validation automatique des modifications locales avant pull: %COMMIT_MESSAGE%"
-)
-
-echo Récupération des modifications distantes...
-git pull origin %BRANCH_NAME% --rebase
-if errorlevel 1 (
-    echo Conflit détecté. Résolvez les conflits manuellement, puis exécutez "git add <fichiers>", "git rebase --continue", et relancez ce script.
-    pause
-    exit /b
 )
 
 echo Ajout des fichiers au commit...
